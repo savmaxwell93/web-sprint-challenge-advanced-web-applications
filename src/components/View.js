8ca878ba43379159810d751b0291daecd8890e65
Savannah <savmaxwell@gmail.com>
2022-01-28 11:14:22 -0700

    articles loading and handleDelete functioning

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 import Article from './Article';
 import EditForm from './EditForm';
@@ -9,7 +10,24 @@ const View = (props) => {
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
 
+    useEffect(() => {
+        axiosWithAuth().get('/articles')
+            .then(resp => {
+                setArticles(resp.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [])
+
     const handleDelete = (id) => {
+        axiosWithAuth().delete(`/articles/${id}`)
+            .then(resp => {
+                setArticles(resp.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
     }
 
     const handleEdit = (article) => {
