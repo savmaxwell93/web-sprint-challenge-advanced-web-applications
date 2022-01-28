6c02b2a6eeb280988f0859782c780d17a578d0ef
Savannah <savmaxwell@gmail.com>
2022-01-28 12:50:55 -0700

    first two tests passing

--- README.md
+++ README.md
@@ -94,8 +94,8 @@ In this project, you will demonstrate your mastery of these skills by creating *
 
 ### Advanced Testing
 > *Add the following tests within Article.test.js.*
-* [ ] Build a test that shows the `Article` component, given the correct props, can render without errors.
-* [ ] Build a test that shows that when a correctly formatted article is passed into the `Article` component, the correct headline, author, summary and body are displayed.
+* [x] Build a test that shows the `Article` component, given the correct props, can render without errors.
+* [x] Build a test that shows that when a correctly formatted article is passed into the `Article` component, the correct headline, author, summary and body are displayed.
 * [ ] The `Article` component should display "Associated Press" when an author attribute is not avalible. Build a test that verifies that that is true.
 * [ ] Build a test that show that when the deleteButton is pressed on an Article, the handleDelete functional property is executed.
 
--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
+const testArticle = {
+    id: 1,
+    headline: 'Headline',
+    author: 'Author',
+    summary: 'Summary',
+    body: 'Body'
+}
+
 test('renders component without errors', ()=> {
+    render(<Article article={{}}/>)
 });
 
-test('renders headline, author from the article when passed in through props', ()=> {
+test('renders headline, author, summary and body from the article when passed in through props', ()=> {
+    render(<Article article={testArticle}/>)
+    const headline = screen.getByTestId(/headline/i);
+    const author = screen.getByTestId(/author/i);
+    const summary = screen.getByTestId(/summary/i);
+    const body = screen.getByTestId(/body/i);
+    expect(headline).toBeInTheDocument();
+    expect(headline).toHaveTextContent(/headline/i);
+    expect(author).toBeInTheDocument();
+    expect(author).toHaveTextContent(/author/i);
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent(/summary/i);
+    expect(body).toBeInTheDocument();
+    expect(body).toHaveTextContent(/body/i);
 });
 
 test('renders "Associated Press" when no author is given', ()=> {
