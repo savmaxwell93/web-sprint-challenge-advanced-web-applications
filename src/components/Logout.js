168479623c0f721ce61e820ff090a627d33b4d18
Savannah <savmaxwell@gmail.com>
2022-01-28 12:11:37 -0700

    logout complete

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
+const Logout = () => {
+    const { push } = useHistory();    
+    useEffect(() => {
+        axiosWithAuth().post('/logout', {})
+            .then(resp => {
+                localStorage.removeItem('token');
+                push('/login')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [])    
     return(<div></div>);
 }
 
